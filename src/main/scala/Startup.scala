e6b01dfe91a5bbd7ce6e38f3544ce43622a0520e
Danylo Kiral <kiral.danylo@gmail.com>
2021-01-11 21:38:43 +0200

    Added WS subscription logic

--- src/main/scala/Startup.scala
+++ src/main/scala/Startup.scala
@@ -1,10 +1,10 @@
 import akka.stream.{FlowShape, Materializer}
-import akka.stream.scaladsl.{Broadcast, BroadcastHub, Flow, GraphDSL, Keep, Merge, Sink, Source}
+import akka.stream.scaladsl.{Broadcast, BroadcastHub, Flow, GraphDSL, Keep, Merge, Sink, Source, Zip, ZipLatest, ZipWith}
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.ws.TextMessage
+import akka.http.scaladsl.model.ws.{Message, TextMessage}
 import dto.{ExposedKeyData, RawKeySearchResult}
 import integration.GitHubKeysSource
-import stages.{ConvertToMessageFlow, DataCollectorSink, KeySearchFlow}
+import stages.{ConvertToMessageFlow, DataCollectorSink, FilterBySubscriptionFlow, KeySearchFlow, MessageToStringFlow}
 import akka.http.scaladsl.server.Directives._
 import GraphDSL.Implicits._
 import akka.actor.ActorSystem
@@ -28,21 +28,35 @@ object Startup extends App {
 
   DataService.initialize()
 
-  val flowGraph = GraphDSL.create[FlowShape[RawKeySearchResult, TextMessage]]() { implicit graphBuilder =>
+  val dataConsumptionGraph = GraphDSL.create[FlowShape[RawKeySearchResult, ExposedKeyData]]() { implicit graphBuilder =>
     val IN = graphBuilder.add(Broadcast[RawKeySearchResult](1))
     val PARSED_DATA = graphBuilder.add(Broadcast[ExposedKeyData](2))
-    val OUT = graphBuilder.add(Merge[TextMessage](1))
+    val OUT = graphBuilder.add(Merge[ExposedKeyData](1))
 
-    IN ~> KeySearchFlow() ~> PARSED_DATA ~> ConvertToMessageFlow() ~> OUT
+    IN ~> KeySearchFlow() ~> PARSED_DATA ~> OUT
                              PARSED_DATA ~> DataCollectorSink()
 
     FlowShape(IN.in, OUT.out)
   }
 
-  val graphSource = GitHubKeysSource()
-    .via(flowGraph)
+  val dataStream = GitHubKeysSource()
+    .via(dataConsumptionGraph)
     .toMat(BroadcastHub.sink)(Keep.right)
     .run
+
+  val socketSubscriptionGraph = GraphDSL.create[FlowShape[Message, TextMessage]]() { implicit graphBuilder =>
+    import GraphDSL.Implicits._
+    val IN = graphBuilder.add(Broadcast[Message](1))
+    val DATA_WITH_SUBSCR = graphBuilder.add(ZipLatest[String, ExposedKeyData]())
+    val OUT = graphBuilder.add(Merge[TextMessage](1))
+
+    IN ~> MessageToStringFlow() ~> DATA_WITH_SUBSCR.in0
+                     dataStream ~> DATA_WITH_SUBSCR.in1
+
+    DATA_WITH_SUBSCR.out ~> FilterBySubscriptionFlow() ~> ConvertToMessageFlow() ~> OUT
+
+    FlowShape(IN.in, OUT.out)
+  }
 
   val serverSource: Source[Http.IncomingConnection, Future[Http.ServerBinding]] =
     Http().newServerAt("localhost", 8080).connectionSource()
@@ -56,7 +70,7 @@ object Startup extends App {
             handleSync(_ => HttpResponse(StatusCodes.OK))
           },
           path("stream") {
-            handleWebSocketMessages(Flow.fromSinkAndSource(Sink.ignore, graphSource))
+            handleWebSocketMessages(Flow.fromGraph(socketSubscriptionGraph))
           }
         )
       }
